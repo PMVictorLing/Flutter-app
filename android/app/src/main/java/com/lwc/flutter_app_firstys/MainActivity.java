3b7417ff39980296e110fd5c0e8be021f6952524
yslingwancai <164660782@qq.com>
2019-03-01 15:53:38 +0800

    first commit

---
+++ android/app/src/main/java/com/lwc/flutter_app_firstys/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lwc.flutter_app_firstys;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
